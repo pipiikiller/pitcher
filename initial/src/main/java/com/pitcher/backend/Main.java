9f064041a856ee3c6e8cac257c3f0bb9e7784a12
pipiikiller <su.win8188@gmail.com>
2018-04-01 16:02:32 +0100

    initial commit

--- initial/src/main/java/com/pitcher/backend/Main.java
+++ initial/src/main/java/com/pitcher/backend/Main.java
@@ -1,4 +1,4 @@
-package pitcher;
+package com.pitcher.backend;
 
 import java.util.Arrays;
 
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Main {
     
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
--- initial/src/main/java/model/Pitch.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**
- * Created by user on 01/04/2018.
- */
-public class Pitch {
-}
