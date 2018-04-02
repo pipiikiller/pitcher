c6d848962eb5fe5731c5209ff3bce8cf3b2c630f
pipiikiller <su.win8188@gmail.com>
2018-04-02 16:43:31 +0100

    PitchDao commit

---
+++ initial/src/main/java/com/pitcher/backend/dao/queries/pitch/PitchDao.java
@@ -0,0 +1,14 @@
+package com.pitcher.backend.dao.queries.pitch;
+
+import com.pitcher.backend.dao.BasicDao;
+import com.pitcher.backend.model.Pitch;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by user on 01/04/2018.
+ */
+@Component
+public interface PitchDao extends BasicDao<Pitch> {
+}
+
+
